3e023ba3e460e3b5562189adce3d80313c2cc195
letnewKid <tevinmcquilkin@gmail.com>
2019-07-02 16:49:05 -0400

    created containers for main views landing page and userdashboard

---
+++ client/containers/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Header from '../components/landingpage/Header';
+
+
+export default function LandingContainer() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
