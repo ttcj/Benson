2e9a32946add6422f5bdf047528f1369b90bd152
Conor Sexton <sextonc@me.com>
2019-07-03 21:19:32 -0400

    Adds additional sever tests
    Co-authored-by: Conor Sexton <sextonc@me.com>
    Co-authored-by: Jarred Harewood <jackhajb@gmail.com>

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,40 +1,19 @@
-const request = require('supertest')
-const app = require('../server/server')
-
-const testSignupData =
-
-describe('Test', () => {
-  test('It should respond with a status code of 200', async () => {
-    const response = await request(app)
-      .get('/test')
-      .expect(200)
-  })
-})
+const request = require('supertest');
+const { app } = require('../server/server');
 
 describe('Root', () => {
   it('should respond the index.html', async () => {
     const response = await request(app)
       .get('/')
       .expect(200)
-      .expect('Content-Type', 'text/html; charset=UTF-8')
-  })
-})
+      .expect('Content-Type', 'text/html; charset=UTF-8');
+  });
+});
 
 describe('Signup', () => {
-  // const validSignupData = {
-  //   name: 'Lucille',
-  //   phoneNumber: 15554443333
-  // }
-
-  // missing a number in the phone number
-  // in the case that a client-side validation fails
-  // const invalidPhoneNumber = {
-  //   name: 'Lucille',
-  //   phoneNumber: 1555444333
-  // }
-  const validSignupData = 'name=%C3%89tienne&phone=1234567890&household=roommates'
-  const invalidPhoneData = 'name=%C3%89tienne&phone=012345678&household=roommates'
-  const missingSignupData = 'phone=0123456789'
+  const validSignupData = 'name=%C3%89tienne&phone=1234567890&household=roommates';
+  const invalidPhoneData = 'name=%C3%89tienne&phone=012345678&household=roommates';
+  const missingSignupData = 'phone=0123456789';
 
   it('should respond with an error if the phone number is invalid', async () => {
     const response = await request(app)
@@ -42,8 +21,8 @@ describe('Signup', () => {
       .send(invalidPhoneData)
       .expect(400)
       .expect('Content-Type', 'text/plain')
-      .expect('invalid phone number')
-  })
+      .expect('invalid phone number');
+  });
 
   it('should respond with an error if a field in the form data is missing', async () => {
     const response = await request(app)
@@ -51,14 +30,27 @@ describe('Signup', () => {
       .send(missingSignupData)
       .expect(400)
       .expect('Content-Type', 'text/plain')
-      .expect('missing name')
-  })
+      .expect('missing name');
+  });
   it('should respond with 200 if valid sign-up data is received', async () => {
     const response = await request(app)
       .post('/')
       .send(validSignupData)
       .expect(200)
       .expect('Content-Type', 'text/plain')
-      .expect('Content-Length', '16')
-  })
-})
+      .expect('Content-Length', '16');
+  });
+});
+
+describe('Adding bills', () => {
+  const invalidBill = {
+    name: 'July Internet',
+  };
+  it('should respond with a 400 status upon receiving invalid bill data', async () => {
+    const response = await request(app)
+      .post('/bills')
+      .send(invalidBill)
+      .set('Content-Type', 'application/json')
+      .expect(400);
+  }, 10000);
+});
